39521f823f6050a3085605dc49e289b962f8e559
U-DESKTOP-7A5237S\Paulo <Paulo Vzquez Acosta>
2022-04-23 21:51:47 +0200

    Feita a lóxica da pantalla inicial
    Cando se selecciona o número de xogadores mostra un toast ao pulsar o
    botón de comezar, no futuro este botón conectará coa seguinte pantalla
    para poñer os nomes dos xogadores

--- android/app/src/main/java/com/app/myapplication/screens/MainActivity.java
+++ android/app/src/main/java/com/app/myapplication/screens/MainActivity.java
@@ -1,14 +1,17 @@
 package com.app.myapplication.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.NumberPicker;
+import android.widget.Toast;
 import com.app.myapplication.R;
 
 
 public class MainActivity extends AppCompatActivity {
     private NumberPicker selectorJugadores;
+    private int numeroJugadores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
         selectorJugadores.setMinValue(2);
         selectorJugadores.setMaxValue(4);
 
+        selectorJugadores.setOnValueChangedListener((numberPicker, i, j) -> {
+            numeroJugadores = selectorJugadores.getValue();
+        });
+
+        final Button botonEmpezar = findViewById(R.id.botonEmpezar);
+
+        botonEmpezar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Jugadores: " + numeroJugadores,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
