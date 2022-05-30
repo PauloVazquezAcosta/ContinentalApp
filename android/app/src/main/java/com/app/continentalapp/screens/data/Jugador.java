bf7050e61f97050c2b506e31c2947b202a055f13
Paulo <paulovazquezacosta@gmail.com>
2022-05-30 19:00:18 +0200

    Creado o Javadoc da clase Jugador

--- android/app/src/main/java/com/app/continentalapp/screens/data/Jugador.java
+++ android/app/src/main/java/com/app/continentalapp/screens/data/Jugador.java
@@ -2,23 +2,44 @@ package com.app.continentalapp.screens.data;
 
 import java.io.Serializable;
 
+/** Clase que representa a cada un dos xogadores da partida creada
+ *
+ */
 public class Jugador implements Serializable {
     private String nombre;
     private int[] puntuacion;
 
+    /** Construtor da clase Jugador
+     *
+     * @param nombre nome do xogador
+     * @param numeroRondas número de rondas para inicializar a súa puntuación
+     */
     public Jugador(String nombre, int numeroRondas) {
         this.nombre = nombre;
         puntuacion = new int[numeroRondas];
     }
 
+    /** Devolve o nome dun xogador específico
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /** Recibe unha puntuación e a posición da ronda e setea a súa puntuación
+     *
+     * @param puntuacion
+     * @param position
+     */
     public void setPuntuacionRonda(int puntuacion, int position) {
         this.puntuacion[position] = puntuacion;
     }
 
+    /** Devolve a puntuación total do xogador
+     *
+     * @return
+     */
     public int getPuntuacionTotal(){
         int puntuacionTotal = 0;
         for (int i = 0; i < puntuacion.length; i++) {
