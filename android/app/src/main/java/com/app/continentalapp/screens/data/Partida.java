2e9f0e89999a7087e90ba1b87d35c3c819efc800
Paulo <paulovazquezacosta@gmail.com>
2022-05-30 19:08:40 +0200

    Creado o Javadoc da clase Partida

--- android/app/src/main/java/com/app/continentalapp/screens/data/Partida.java
+++ android/app/src/main/java/com/app/continentalapp/screens/data/Partida.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/** Clase que representa á partida creada para levar a puntuación
+ *
+ */
 public class Partida implements Serializable {
 
     private int numeroJugadores;
@@ -13,6 +16,10 @@ public class Partida implements Serializable {
     private String[] jugadasRondas;
     private int rondaActual;
 
+    /** Construtor da clase Partida, recibindo o número de xogadores xa inicializa o resto de parámetros
+     *
+     * @param numeroJugadores cantidade de xogadores da partida
+     */
     public Partida(int numeroJugadores) {
         this.numeroJugadores = numeroJugadores;
         jugadores = new Jugador[numeroJugadores];
@@ -24,26 +31,50 @@ public class Partida implements Serializable {
         rondaActual = 1;
     }
 
+    /** Devolve o número de xogadores da partida
+     *
+     * @return
+     */
     public int getNumeroJugadores() {
         return numeroJugadores;
     }
 
+    /** Devolve o listado de xogadores da partida
+     *
+     * @return
+     */
     public Jugador[] getJugadores() {
         return jugadores;
     }
 
+    /** Recibe o listado de xogadores e asígnao á partida
+     *
+     * @param jugadores array de obxectos Jugador
+     */
     public void setJugadores(Jugador[] jugadores) {
         this.jugadores = jugadores;
     }
 
+    /** Recibe a posición e o nome e ubica ao xogador nesa posición
+     *
+     * @param position posición do xogador dentro da partida
+     * @param nombre nome do xogador
+     */
     public void setJugador(int position, String nombre) {
         jugadores[position] = new Jugador(nombre, numeroRondas);
     }
 
+    /** Devolve un array co texto indicativo da posición de cada xogador
+     *
+     * @return
+     */
     public String[] getTextoTituloJugadores() {
         return textoTituloJugadores;
     }
 
+    /** Setea o texto indicativo da posición de cada un dos xogadores
+     *
+     */
     private void setTextoTituloJugadores() {
         for (int i = 0; i < numeroJugadores; i++) {
             int numeroJugador = i + 1;
@@ -51,10 +82,17 @@ public class Partida implements Serializable {
         }
     }
 
+    /** Devolve un array co listado de xogadas da partida que se vai xogar
+     *
+     * @return
+     */
     public String[] getJugadasRondas() {
         return jugadasRondas;
     }
 
+    /** Setea o listado de xogadas da partida a xogar
+     *
+     */
     private void setJugadasRondas() {
         jugadasRondas[0] = "Un trío - 4 cartas";
         jugadasRondas[1] = "Una escalera - 5 cartas";
@@ -68,20 +106,36 @@ public class Partida implements Serializable {
         jugadasRondas[9] = "Cuatro tríos - 13 cartas";
     }
 
+    /** Devolve a ronda actual na que se atopa a partida
+     *
+     * @return
+     */
     public int getRondaActual() {
         return rondaActual;
     }
 
+    /** Suma unha ronda á partida para iniciar a seguinte xogada
+     *
+     */
     public void sumarRonda() {
         rondaActual++;
     }
 
+    /** Devolve o número de rondas totais da partida
+     *
+     * @return
+     */
     public int getNumeroRondas() {
         return numeroRondas;
     }
 
+    /** Devolve un array de xogadores en orde para sinalar ao gañador
+     *
+     * @return
+     */
     public Jugador[] getRanking() {
 
+        // Compara os xogadores pola súa puntuación total e ordea o array en función diso
         Comparator comparadorPuntuacion = Comparator.comparing(Jugador::getPuntuacionTotal);
         Arrays.sort(jugadores, comparadorPuntuacion);
         return jugadores;
